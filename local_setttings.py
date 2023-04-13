2a147ba779ac9565deb388ec068ae3d1fd3fcd3e
Elvira Maestro <paloma.elviramaestro@capgemini.com>
2023-04-13 17:29:33 +0200

    local_settings

--- local_setttings.py
+++ local_setttings.py
@@ -6,42 +6,26 @@ import openpyxl
 from data import change_colour, change_column_AdditionalNotes, change_column_TeamRequestComment1, delete_rows_LeadPracticeArea, delete_rows_TeamRequestStatu, find_column_Team, find_column_lead, format_column, format_condition, format_condition_iter2, insert_column, remove, unmerge_cells
 from miargparse import parser
 from openpyxl.styles import PatternFill 
-#Damos la localización del fichero de entrada
-#ruta_input = "C:\\Users\\lgordoga\\L99PYTHON\\SPRINT_0\\in_corto.xlsx"
-#Damos la localización del fichero de salida
-#ruta_output = "C:\\Users\\lgordoga\\L99PYTHON\\SPRINT_0\\out.xlsx"
-# Creamos objeto wb (libro) de tipo workbook y lo cargamos con lo del excel
+
 args = parser.parse_args()
-#ruta_input = args.ruta_input
 print(args)
 
 wb = openpyxl.load_workbook(args.ruta_input)
-# Creamos objeto ws (hoja), siendo la hoja activa
-#ws = wb.active 
 ws = wb['Retain Report']
-#print('la celda A1 es:' ,ws['A1'].value)
 
-#Metodo que desmergea las celdas de las filas a eliminar
 unmerge_cells(ws)
 
-# Metodo que sirve para borrar todas las filas de 1 a 11
-#for row in ws: 
 remove(ws,1,11)
 
-# Metodo que elimina el color amarillo de todas la celdas que sean amarillas 
 change_colour(ws)
 
-#Metodo que inserta una columna nueva detrás de la columna H poniendo la cabecera y copiando el formato de la columna H
 insert_column(ws, colNr=9, headerRow=1, headerVal='FTES. Pdtes.')
 
 #Metodo que da formato a la columna que se ha creado usando range
 format_column(ws, colNr= 9)
-#Metodo que da formato a la columna que se ha creado utilizando iter_cols
-#format_column_iter(ws, colNr= 9)
 
 #Metodo que da formato a la columna que se ha creado según criterio  
 color_yellow = PatternFill(start_color="FFFFFF99", end_color="FFFFFF99", fill_type = "solid")
-
 format_condition_iter2(ws, colNr= 9, condition="CRITICA", color=color_yellow)
 
 # Metodo que inserta una columna nueva detrás de la columna H poniendo la cabecera y copiando el formato de la columna H
